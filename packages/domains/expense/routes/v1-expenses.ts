3f935e52d290a9f2a01d6b860ed571e2591fa2c4
Riccardo Amadio <riccardo.amadio1@gmail.com>
2022-02-11 12:22:02 +0100

    Implement get-expenses by userId Using the same settings features created for the get-expenses I added the filter for userId.

--- packages/domains/expense/routes/v1-expenses.ts
+++ packages/domains/expense/routes/v1-expenses.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
 import { to } from '@nc/utils/async';
-import { getExpenses } from '../services/get-expenses'
+import { getExpenses,getExpensesByUserId } from '../services/get-expenses'
 export const router = Router();
 
 router.get('/get-expenses', async (req, res, next) => {
+    console.log(req.query)
     const [error, response] = await to(getExpenses(req.query));
-  
-    if (error) 
-        return next(error);    
-    else  
+
+    if (error)
+        return next(error);
+    else
         return res.send(response);
+});
+
+router.get('/get-expenses/:user_id', async (req, res, next) => {
+    const [error, response] = await to(getExpensesByUserId(req.query,req.params.user_id));
+  
+    if (error) {
+      return next(error);
+    }
+  
+    return res.send(response);
   });
--- packages/domains/expense/services/get-expenses.ts
+++ packages/domains/expense/services/get-expenses.ts
@@ -18,8 +18,29 @@ const getExpenses = async (query: undefined): Promise<any> => {
     throw NotFound(`Could not find expenses `);
   }
 
-  return { expenses, pagination };
+  return { expenses, pagination, sortings, filters };
 }
 
 
-module.exports = {  getExpenses };
+const getExpensesByUserId = async ( query: undefined,user_id:string): Promise<any> => {
+  const pagination = setPagination(query);
+  const sortings = setSortings(query);
+  const filters = setFilters(query);
+  filters.where.user_id=user_id;
+  const settings = {...pagination,...sortings,...filters};
+  const [dbError, expenses] = await to(Expenses.find(settings));
+  if (dbError !== null) {
+    throw InternalError(`Error fetching data from the DB: ${dbError.message}`);
+  }
+
+  if (!expenses) {
+    throw NotFound(`Could not find expenses with user_id ${user_id}`);
+  }
+
+  return { expenses, pagination, sortings, filters };
+}
+
+
+
+
+module.exports = {  getExpenses,getExpensesByUserId };
